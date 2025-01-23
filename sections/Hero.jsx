274ba2cbb033d4db32006fa720ea901383a82f84
Collin <cporter.dev@gmail.com>
2025-01-23 14:53:34 -0700

    fixed issue with tailwind.config, added navbar content & began hero section build

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import styles from "../styles/index.js";
+import { slideIn, staggerContainer, textVariant } from "../utils/motion";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+      <motion.div
+        variants={staggerContainer}
+        intial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <div className="flex flex-col justify-center items-center relative z-10">
+          <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+            Metaverse
+          </motion.h1>
+          <motion.div
+            variants={textVariant(1.2)}
+            className="flex flex-row justify-center items-center"
+          >
+            <h1 className={styles.heroHeading}>Ma</h1>
+            <div className={styles.heroDText} />
+            <h1 className={styles.heroHeading}>Ness</h1>
+          </motion.div>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
