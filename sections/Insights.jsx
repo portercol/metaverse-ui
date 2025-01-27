9f226f3b39cbbe207ff75f354f194567b1f5607a
Collin <cporter.dev@gmail.com>
2025-01-27 14:46:52 -0700

    added insights section and insightCard component

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import styles from "../styles/index.js";
+import { staggerContainer } from "../utils/motion";
+import { InsightCard, TypingText, TitleText } from "../components";
+import { insights } from "@/constants";
 
 const Insights = () => {
   return (
-    <div>Insights</div>
-  )
-}
+    <section className={`${styles.paddings} relative z-10`}>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: "false", amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col `}
+      >
+        <TypingText title="| Insights" textStyles="text-center"/>
+        <TitleText title="Insights about the Metaverse" textStyles="text-center" />
+        <div className="mt-[50px] flex flex-col gap-8">
+          {insights.map((insight, index) => (
+            <InsightCard key={`insight-${index}`} {...insight} index={index + 1} />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 
-export default Insights
+export default Insights;
